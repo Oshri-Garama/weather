cad443d12ab8b9b6de4a1141886a23ba5a8bbe0e
oshripickapier <oshri@pickapier.com>
2020-04-29 19:42:47 +0300

    ex1 except styling is done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import './styles/index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiWeatherKey = "a7e9b7cd83166d594a42858290bbc541";
+
+class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: props.city || "Tel Aviv",
+      data: {},
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      city: event.target.value,
+    });
+  }
+
+  handleSearch() {
+    const units = "metric";
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&units=${units}&appid=${apiWeatherKey}`;
+
+    axios.get(url).then((res) => {
+      if (res.status === 200) {
+        this.setState({
+          data: res.data,
+        });
+      }
+    });
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div>
+        <input id='searchBar' type="search" onChange={this.handleChange}></input>
+        <button id='searchButton' type="submit" onClick={this.handleSearch}>
+          Search
+        </button>
+        {data.main ? (
+          <div className='temp'>
+            {data.name}, {data.sys.country}
+            <br/>
+            <br/>
+            {data.main.temp}&deg;
+            <br/>
+            {data.weather[0].description.toUpperCase()}
+            <br/>
+            feels like {data.main.feels_like}&deg;
+          </div>
+        ) : (
+          <div className='temp'>Loading...</div>
+        )}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Weather;
