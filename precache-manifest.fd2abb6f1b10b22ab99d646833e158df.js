3683fc37bd3fe6f2e5132828494eda610b8f80ec
oshripickapier <oshri@pickapier.com>
2020-05-01 16:40:32 +0300

    Updates

---
+++ precache-manifest.fd2abb6f1b10b22ab99d646833e158df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf9c2b02db8501dd99b709ad16815c3",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "8197d2056b1e8b8a754c",
+    "url": "/weather/static/css/main.69b5debf.chunk.css"
+  },
+  {
+    "revision": "05b4afab8ffb38ec9a01",
+    "url": "/weather/static/js/2.da4bc9c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.da4bc9c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8197d2056b1e8b8a754c",
+    "url": "/weather/static/js/main.4a9f42d0.chunk.js"
+  },
+  {
+    "revision": "e86e020f803953394786",
+    "url": "/weather/static/js/runtime-main.184f8d0f.js"
+  }
+]);
